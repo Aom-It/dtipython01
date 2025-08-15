ae1dfeb8e0859c157bbc57ccd69e76e2d9c67d8e
Aom-It <gramgramg4@gmail.com>
2025-08-15 16:08:06 +0700

    v.2 code 01-04 but 05 no ok

--- workshop01.py
+++ workshop01.py
@@ -1,5 +1,5 @@
 import sys
-from rich import print
+from rich import print 
 from time import sleep 
 import time
 
@@ -13,7 +13,7 @@ def printLyrics():
         ("I wanna go for a ride",0.068),#6
         ("Hop in the music and",0.07),#7
         ("Rock your body",0.08),#8
-        ("Rock that body",0.069)#9
+        ("Rock that body",0.069),#9
         ("come on, come on",0.035),#10
         ("Rock that body",0.05),#11
         ("(Rock that body)",0.03),#12
---
+++ workshop03.py
@@ -0,0 +1,19 @@
+# กำลังแสดงผลช้อมูลใดๆ ที่หน้าจอ ใช่ฟังก์ฃัน print()
+# string text
+print("hello")
+print("hi")
+print('Nigga')
+print("fat")
+
+# number ตัวเลข
+print(4343344) #integer/int
+print(12.22222) #float
+
+#boolean ตรรกะ 2 true/false
+print(True)
+print(False)
+
+#expression นิพจน์ คือ อะไรก็ตามที่มี process การทำงาน เช่น การคำนวณ,เปรียบเทียบ
+print(10 + 20)
+print(25 > 50)
+print("bangna" <= "Bangpoo")
